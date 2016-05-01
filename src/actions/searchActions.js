70a3b7798b829580d4c3d61f24927f4c6a56d2fa
Biletnikoff <bogeresanders@gmail.com>
2016-04-30 22:30:44 -0700

    create action to get data from backend api call

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,5 +1,10 @@
-//import { createAction } from 'redux-actions';
+import { createAction } from 'redux-actions';
+import backend from '../service/backend.js';
 
+
+export default createAction(
+  'GET_CARDATA',
+   async (searchQuery) => await backend.getCarData(searchQuery));
 
 // Actions:
 // Logout
