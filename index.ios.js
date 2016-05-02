d0d31ef89afb91411b6a0b61ad5fb14fee8f9f7d
John Zuccarelli <zucca22@gmail.com>
2016-05-02 12:11:21 -0700

    login partial

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,18 @@
-import './src';
+'use strict';
+var React = require('react-native');
+var Login = require('./src/components/Login');
+
+class CarlyApp extends React.Component {
+	render() {
+		return (
+			<React.NavigatorIOS 
+				initialRoute={{
+					title: 'Carly',
+					component: Login
+				}}/>
+		);
+	}
+}
+
+React.AppRegistry.registerComponent('CarlyApp', function() {return CarlyApp})
+
---
+++ src/components/Login.js
@@ -0,0 +1,78 @@
+var React = require('react-native');
+
+var {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicatorIOS
+} = React;
+
+var styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#48BBEC'
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 25,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  searchInput: {
+    height: 50,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+});
+
+
+class Login extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			email: '',
+			password: '',
+			isLoading: false,
+			error: false
+		}
+	}
+
+	render() {
+		return (
+			<View style={styles.mainContainer}>
+				<Text style={styles.title}>Carly</Text>
+			</View>
+		);
+	}
+}
+
+module.exports = Login;
