cb53be823e63f08aedd30b4e5fae34f54791a7c7
John Zuccarelli <zucca22@gmail.com>
2016-05-02 14:07:23 -0700

    Login partial

--- index.ios.js
+++ index.ios.js
@@ -1,18 +1,1 @@
-'use strict';
-var React = require('react-native');
-var Login = require('./src/components/Login');
-
-class CarlyApp extends React.Component {
-	render() {
-		return (
-			<React.NavigatorIOS 
-				initialRoute={{
-					title: 'Carly',
-					component: Login
-				}}/>
-		);
-	}
-}
-
-React.AppRegistry.registerComponent('CarlyApp', function() {return CarlyApp})
-
+import './src';
