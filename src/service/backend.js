390bb7adde0103a98e2701ab31a76f16dce33668
Biletnikoff <bogeresanders@gmail.com>
2016-05-03 16:41:15 -0700

    refactor api call

--- src/service/backend.js
+++ src/service/backend.js
@@ -7,13 +7,16 @@ const {
 
 module.exports = {
   getCarData(searchQuery) {
-    return fetch(autotraderExtractor+`&url=http%3A%2F%2Fwww.autotrader.com%2Fcars-for-sale%2FUsed%2BCars%2F
-      ${searchQuery.carMake}%2F
-      ${searchQuery.carModel}%2F
-      ${searchQuery.location.city}%2B
-      ${searchQuery.location.zipcode}%3FendYear%3D
-      ${searchQuery.location.maxPrice}%26`, {
-        method: 'GET',
-      }).then((res) => res.json());
-  }
-};
+    console.log(searchQuery.model);
+    return fetch(autotraderExtractor+`&url=http%3A%2F%2Fwww.autotrader.com%2Fcars-for-sale%2FUsed%2BCars%2F${searchQuery.carMake}%2F${searchQuery.model}%2F${searchQuery.zipcode}%3FendYear%3D${searchQuery.sliderRight}%26`, {
+      method: 'GET',
+    }).then((response)=> {
+    return response.json();
+    }).then((data) => {
+    console.log(data);
+    //  callback(data.extractorData.data[0].group);
+    }).catch((err)=> {
+      console.log('Error ', err);
+    });
+    }
+    };
