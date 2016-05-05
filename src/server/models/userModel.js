f8522bca5af7ff6030517c1e64dd5a3e5ee0e51b
David Gilroy <dgilroy77@gmail.com>
2016-05-04 18:47:31 -0700

    Added signup server route, successful post test on postman

--- src/server/models/userModel.js
+++ src/server/models/userModel.js
@@ -7,6 +7,12 @@ module.exports = {
       db.query(queryUser, function(err, results) {
         callback(err, results);
       });
+    },
+    get: function (user,callback) {
+      var query = 'Select email from Users where email = "'+ user.email +'" and password = "'+user.password+'"';
+      db.query(query, function(err, results) {
+        callback(err, results);
+      });
     }
   }
 }
--- src/server/routes.js
+++ src/server/routes.js
@@ -2,4 +2,5 @@ var userController = require('./controllers/userController.js');
 
 module.exports = function(app) {
   app.post('/api/users/signup', userController.signup);
+  app.post('/api/users/login', userController.login);
 }
