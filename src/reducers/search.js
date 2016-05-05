15e94c2a807e29e2934749515246d6f1350550a6
Biletnikoff <bogeresanders@gmail.com>
2016-05-04 20:41:51 -0700

    edit payload properties

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,11 +1,32 @@
 import { handleActions } from 'redux-actions';
 
 const initialState = {
-  cars: [],
+  query: {
+    carMake: 'cadillac',
+    modelIndex: 3,
+    value: 0,
+    sliderLeft:1990,
+    sliderRight:2017,
+    minPrice:1000,
+    maxPrice:150000,
+    zipcode:'',
+    model: ''
+  },
   loading: true,
 };
 
 
 export default handleActions({
-  GET_CARDATA: (state, { payload }) => ({ ...state, cars: payload, loading: false })
+  GET_CARDATA: (state, { payload }) => ({
+    ...state,
+    carMake: payload.carmake || 'cadillac',
+    modelIndex: payload.modelIndex || 3,
+    value: payload.value || 0,
+    sliderLeft: payload.sliderLeft || 1990,
+    sliderRight: payload.sliderRight || 2017,
+    minPrice: payload.minPrice || 1000,
+    maxPrice: payload.maxPrice || 150000,
+    zipcode: payload.zipcode || '',
+    model: payload.model || ''
+  })
 }, initialState);
