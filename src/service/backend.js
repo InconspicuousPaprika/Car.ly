48132a629e5a338fd578ec6a7adbb0eaca46dc65
Biletnikoff <bogeresanders@gmail.com>
2016-05-06 21:35:06 -0700

    small string intepolation change

--- src/service/backend.js
+++ src/service/backend.js
@@ -1,5 +1,5 @@
 // import config from '../../config.js';
-import Search from '../components/Counter.js';
+import Search from '../components/Search.js';
 // const {
 //   autotraderExtractor,
 // } = config;
@@ -11,9 +11,9 @@ module.exports = {
     return fetch(autotraderExtractor+`&url=http%3A%2F%2Fwww.autotrader.com%2Fcars-for-sale%2FUsed%2BCars%2F${searchQuery.carMake}%2F${searchQuery.model}%2F${searchQuery.zipcode}%3FendYear%3D${searchQuery.sliderRight}%26`, {
       method: 'GET',
     }).then((response)=> {
-    return response.json();
+      return response.json();
     }).then((data) => {
-    console.log(data);
+      console.log(data);
     //  callback(data.extractorData.data[0].group);
     }).catch((err)=> {
       console.log('Error ', err);
