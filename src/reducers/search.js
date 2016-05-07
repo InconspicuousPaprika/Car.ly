417b8dcb557f5546eb51cf59286bffa8f9d1b390
Biletnikoff <bogeresanders@gmail.com>
2016-05-06 21:34:08 -0700

    add new action and set car makes on state object

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,31 +1,67 @@
 import { handleActions } from 'redux-actions';
 
 const initialState = {
-  query: {
-    carMake: 'cadillac',
-    value: 0,
-    startYear:1990,
-    endYear:2017,
-    minPrice:1000,
-    maxPrice:150000,
-    zipcode:'',
-    model: false
-  },
-  loading: true,
+  carMake: 'cadillac',
+  value: 0,
+  startYear:1990,
+  endYear:2017,
+  minPrice:1000,
+  maxPrice:150000,
+  zipcode:'',
+  model: 'Fleetwood',
+  loading: false,
+  modelIndex: 3,
+  searchResults:{},
+  allCars: {
+    amc: {
+      name: 'AMC',
+      models: ['AMX', 'Concord', 'Eagle', 'Gremlin', 'Matador', 'Pacer'],
+    },
+    alfa: {
+      name: 'Alfa-Romeo',
+      models: ['159', '4C', 'Alfasud', 'Brera', 'GTV6', 'Giulia', 'MiTo', 'Spider'],
+    },
+    aston: {
+      name: 'Aston Martin',
+      models: ['DB5', 'DB9', 'DBS', 'Rapide', 'Vanquish', 'Vantage'],
+    },
+    audi: {
+      name: 'Audi',
+      models: ['90', '4000', '5000', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'Q5', 'Q7'],
+    },
+    austin: {
+      name: 'Austin',
+      models: ['America', 'Maestro', 'Maxi', 'Mini', 'Montego', 'Princess'],
+    },
+    borgward: {
+      name: 'Borgward',
+      models: ['Hansa', 'Isabella', 'P100'],
+    },
+    buick: {
+      name: 'Buick',
+      models: ['Electra', 'LaCrosse', 'LeSabre', 'Park Avenue', 'Regal',
+      'Roadmaster', 'Skylark'],
+    },
+    cadillac: {
+      name: 'Cadillac',
+      models: ['Catera', 'Cimarron', 'Eldorado', 'Fleetwood', 'Sedan de Ville'],
+    },
+    chevrolet: {
+      name: 'Chevrolet',
+      models: ['Astro', 'Aveo', 'Bel Air', 'Captiva', 'Cavalier', 'Chevelle',
+      'Corvair', 'Corvette', 'Cruze', 'Nova', 'SS', 'Vega', 'Volt'],
+    }
+  }
+
 };
 
 
 export default handleActions({
   GET_CARDATA: (state, { payload }) => ({
     ...state,
-    carMake: payload.carmake,
-    value: payload.value,
-    sliderLeft: payload.sliderLeft,
-    sliderRight: payload.sliderRight,
-    minPrice: payload.minPrice,
-    maxPrice: payload.maxPrice,
-    zipcode: payload.zipcode,
-    model: payload.model
+    ...payload.payload
   }),
-  SET_QUERY: (state, payload) => ({ ...state, query:Object.assign({}, state.query, payload.payload )})
+  SET_QUERY: (state, payload) => ({
+    ...state,
+    ...payload.payload })
 }, initialState);
