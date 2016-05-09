0c4f728b72490ec77fb6905994c0d9abc2263e98
Claire Hsu <clairehsu86@gmail.com>
2016-05-09 12:18:06 -0700

    display results

---
+++ src/components/resultsList.js
@@ -0,0 +1,82 @@
+import React, {
+  StyleSheet,
+  Component,
+  View,
+  Text,
+  Image,
+  ListView,
+  ScrollView,
+  TouchableHighlight
+} from 'react-native';
+
+export default class PhotoList extends Component {
+
+  constructor(props){
+    super(props);
+
+  }
+
+  convertScale(url) {
+    // let newurl = url.replace(new RegExp('scaler\/\d\d\d\/\d\d\d'), "scaler/100/75"); ReGex is not happening
+    let newurl = 'http://images.autotrader.com/scaler/400/300/'+url.slice(42);
+    // console.log(newurl);
+    console.log('begin printing this.props');
+    console.log(this.props);
+    console.log('done printing this.props');
+    return newurl;
+
+  }
+
+  render() {
+    return (
+      // <View>
+      // <Text>words</Text>
+      <ScrollView>
+        {
+          this.props.photos.map((item, index) => {
+            return (
+              <Image
+                key={`PhotoItem_${item['VEHICLETITLE LINK'].title}_${index}`}
+                style={styles.image}
+                source={{uri: this.convertScale(item.image[0].src)}}
+              />
+            );
+          })
+        }
+        {
+         (() => {
+           if (this.props.status === 'DONE') {
+             return (
+               <TouchableHighlight style={styles.button} onPress={this.props.actions.searchNextPageAction}>
+                <Text style={styles.buttonText}>Load More</Text>
+              </TouchableHighlight>
+             );
+           }
+         })()
+       }
+      </ScrollView>
+      // </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width:414,
+    height:310,
+    flex: 1
+  },
+  button: {
+    height: 50,
+    backgroundColor: '#3D3D46',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    paddingTop: 10,
+    paddingBottom:10
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#fff',
+    alignSelf: 'center'
+  }
+});
