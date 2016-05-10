d612b28c579b3eefec99ad87ec73cc7dbb1eed9f
Biletnikoff <bogeresanders@gmail.com>
2016-05-09 21:24:13 -0700

    add a master styles folder

---
+++ src/styles/SearchGlobalStyles.js
@@ -0,0 +1,30 @@
+
+import React from 'react-native';
+const {
+  StyleSheet,
+} = React;
+
+import globalVariables from '../globalVariables.js';
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20,
+  },
+
+  label: {
+    fontSize: 12,
+    color: '#666',
+    paddingBottom: 5
+  },
+
+  innerBox: {
+    flex: 1,
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+});
+
+module.exports = styles
