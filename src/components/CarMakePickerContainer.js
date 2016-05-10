656157dc185063126818d785ce2bd34225fdfa96
Biletnikoff <bogeresanders@gmail.com>
2016-05-10 15:06:40 -0700

    Create container for both car make and model pickers

--- src/components/CarMakePickerContainer.js
+++ src/components/CarMakePickerContainer.js
@@ -7,18 +7,18 @@ const {
   View,
 } = React;
 
-import globalVariables from '../styles/globalVariables.js';
-import CarMakePickers from './CarMakePickers.js';
-import CarModelPickers from './CarModelPickers.js';
+import globalStyles from '../styles/SearchGlobalStyles.js';
+import CarMakePicker from './CarMakePicker.js';
+import CarModelPicker from './CarModelPicker.js';
 
-const CarMakePickerContainer(props) => ({
+const CarMakePickerContainer = (props) => ({
   render(){
     return (
       <View style={globalStyles.container}>
         <Text style={globalStyles.label}>Chose your car</Text>
         <View style={globalStyles.innerBox}>
-          <CarMakePickers label='Make' onChange={this.handleStartChange} value={this.props.value[0]} />
-          <CarModelPickers label='Model' onChange={this.handleEndChange} value={this.props.value[1]} />
+          <CarMakePicker label='Make' />
+          <CarModelPicker label='Model' />
         </View>
       </View>
     );
@@ -32,7 +32,16 @@ const CarMakePickerContainer(props) => ({
     // this.props.onChange('priceRange', [this.props.value[0], value]);
   }
 
-})
+});
+
+const styles = StyleSheet.create({
+  divider: {
+    height: 20,
+    width: 1,
+    backgroundColor: '#ccc'
+  },
+});
+
 
 
 export default CarMakePickerContainer;
