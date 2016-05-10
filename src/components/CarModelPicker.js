b231bb42da2d453425e0dd50c062d229aad4d356
Biletnikoff <bogeresanders@gmail.com>
2016-05-10 15:14:20 -0700

    create deep nested child component to handle dispatch of data

--- src/components/CarModelPicker.js
+++ src/components/CarModelPicker.js
@@ -6,30 +6,54 @@ const {
   Text,
   View,
   PickerIOS,
+  PickerItemIOS,
+  Component
 } = React;
+import setQueryAction from '../actions/setQueryAction.js';
+import searchActions from '../actions/searchActions.js';
+import MultiSlider from 'react-native-multi-slider';
+import Immutable from 'immutable';
+import { connect } from 'react-redux';
 import globalVariables from '../styles/globalVariables.js'
 
-const CarModelPickers() => ({
+@connect(state => ({
+  query: state.search,
+  CAR_MAKES_AND_MODELS: state.search.allCars
+}))
+export default class CarModelPicker extends Component {
+  static propTypes = {
+    dispatch: React.PropTypes.func.isRequired,
+    query: React.PropTypes.object.isRequired
+  };
+
   render() {
-    <PickerIOS
-      selectedValue={query.modelIndex}
-      key={query.carMake}
-      onValueChange={(index) =>
-        dispatch(setQueryAction({
-          modelIndex:index,
-          model:make.models[index]
-        }))}
-    >
-      {CAR_MAKES_AND_MODELS[query.carMake].models.map((modelName, modelIndex) => (
-        <PickerItemIOS
-          key={ `${query.carMake} ${modelIndex}` }
-          value={modelIndex}
-          label={modelName}
-        />
-      ))}
-    </PickerIOS>
+    const { query, dispatch, CAR_MAKES_AND_MODELS } = this.props;
+    const make = CAR_MAKES_AND_MODELS[query.carMake];
+    return (
+      <View style={styles.container}>
+        <Text style={styles.label}>{this.props.label}</Text>
+
+      <PickerIOS
+        selectedValue={query.modelIndex}
+        key={query.carMake}
+        onValueChange={(index) =>
+          dispatch(setQueryAction({
+            modelIndex:index,
+            model:make.models[index]
+          }))}
+      >
+        {CAR_MAKES_AND_MODELS[query.carMake].models.map((modelName, modelIndex) => (
+          <PickerItemIOS
+            key={ `${query.carMake} ${query.modelIndex}` }
+            value={modelIndex}
+            label={modelName}
+          />
+        ))}
+      </PickerIOS>
+    </View>
+    )
   }
-});
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -47,5 +71,3 @@ const styles = StyleSheet.create({
     color: globalVariables.green,
   },
 });
-
-export default CarModelPickers;
