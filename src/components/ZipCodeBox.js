89e150c371e179e429d9c7f366e9bba68ad916b4
Biletnikoff <bogeresanders@gmail.com>
2016-05-11 14:43:50 -0700

    Refactor zipcode input to allow adding a remove of zip from the ui

---
+++ src/components/ZipCodeBox.js
@@ -0,0 +1,64 @@
+import React from 'react-native';
+const {
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Component,
+  TextInput
+} = React;
+import setQueryAction from '../actions/setQueryAction.js';
+import Immutable from 'immutable';
+import { connect } from 'react-redux';
+import globalVariables from '../styles/globalVariables.js'
+import globalStyles from '../styles/SearchGlobalStyles.js'
+
+const ZipCodeBox = React.createClass({
+  getDefaultProps() {
+    return {
+      value: null,
+      index: 0,
+      onRemove() {}
+    };
+  },
+  render() {
+    return (
+      <View style={styles.zipView}>
+        <Text style={styles.zipText}>{this.props.value}</Text>
+        <TouchableOpacity onPress={this.handleClear}>
+          <Image style={styles.clearBtn} source={require('../images/Clear.png')} />
+        </TouchableOpacity>
+      </View>
+    );
+  },
+
+  handleClear() {
+    this.props.onRemove(this.props.index);
+  }
+});
+const styles = StyleSheet.create({
+  zipView: {
+    backgroundColor: '#F9F9F9',
+    borderRadius: 2,
+    padding: 10,
+    paddingLeft: 15,
+    paddingRight: 30,
+    margin: 5,
+  },
+
+  zipText: {
+    color: globalVariables.green,
+    fontSize: 18,
+  },
+
+  clearBtn: {
+    position: 'absolute',
+    right: 10,
+    top: 14,
+    width: 14,
+    height: 14,
+    opacity: 0.5
+  },
+
+});
