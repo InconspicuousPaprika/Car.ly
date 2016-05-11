2f85216ead22954c88c90bb19f5e3efc84610752
Biletnikoff <bogeresanders@gmail.com>
2016-05-11 14:43:15 -0700

    Refactor zipcode input

--- src/components/ZipcodeEntry.js
+++ src/components/ZipcodeEntry.js
@@ -1,5 +1,27 @@
+import React from 'react-native';
+const {
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Component,
+  TextInput
+} = React;
+import setQueryAction from '../actions/setQueryAction.js';
+import Immutable from 'immutable';
+import { connect } from 'react-redux';
+import globalVariables from '../styles/globalVariables.js'
+import globalStyles from '../styles/SearchGlobalStyles.js'
+@connect(state => ({
+  query: state.search,
+}))
+
+export default class ZipcodeEntry extends Component{
+
 <TextInput
   style={ { height: 40, width:100, alignSelf:'center', borderColor: 'gray', borderWidth: 1 } }
   onChangeText={(zipcode) => dispatch(setQueryAction({ zipcode }))}
   value={query.zipcode}
 />
+}
