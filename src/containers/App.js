31ccb6c779f26b20901abe892cbff45139302aad
John Zuccarelli <zucca22@gmail.com>
2016-05-12 17:31:31 -0700

    validate login parameters.

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,10 +22,10 @@ import activeSearch from '../assets/images/TabBar-House-Icon-Active@3x.png'
 
 const scenes = Actions.create(
   <Scene key="root" >
-    <Scene type="replace" key="Search" initial={true} component={Search} />
+    <Scene type="replace" key="Login" initial={true} component={Login} />
     <Scene type="replace" key="SignUp" direction="horizontal" component={Signup} />
     <Scene type="replace" key="Favorites" component={Search}/>
-    <Scene type="replace" key="Login" component={Login} />
+    <Scene type="replace" key="Search" component={Search} />
   </Scene>
 );
 
--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,13 +1,11 @@
 import { handleActions } from 'redux-actions';
 import {SIGN_IN} from '../actions/loginActions.js';
-import {SEND_POST_LOGIN} from '../actions/sendPostDataLoginAction.js'
+import {SEND_POST_LOGIN, VALID_USER, INVALID_USER} from '../actions/sendPostDataLoginAction.js'
 
 const initialState = {
   email: '',
   password: '',
-  isLoading: false,
-  error: false,
-  isPassword: false
+  isValidLogin: false,
 }
 
 export default handleActions ({
@@ -16,11 +14,21 @@ export default handleActions ({
     ...payload.payload
   }),
 
-	SEND_POST_LOGIN: (state, payload) => ({
-		...state,
-		...payload.payload
-	})
-}, initialState);
+  SEND_POST_LOGIN: (state, payload) => ({
+    ...state,
+    ...payload.payload
+  }),
+
+  VALID_USER: (state, payload) => ({
+    ...state,
+    ...payload.validLogin
+  }),
+
+  INVALID_USER: (state, payload) => ({
+    ...state,
+    ...payload.payload
+  }),
+}, initialState)
 
 
 
