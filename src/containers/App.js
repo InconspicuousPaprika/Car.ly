249e26f4ccde02e3cb8e8306b6dd215cf2925781
Biletnikoff <bogeresanders@gmail.com>
2016-05-13 16:46:06 -0700

    merge fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,17 +22,10 @@ import activeSearch from '../assets/images/TabBar-House-Icon-Active@3x.png'
 
 const scenes = Actions.create(
   <Scene key="root" >
-<<<<<<< 1c661f4e5b019c7a1d9a97015f7b0c1f7453c2c6
-    <Scene type="replace" key="Signup" initial={true} component={Signup} />
-    <Scene type="replace" key="Login" direction="horizontal" component={Login} />
-    <Scene type="replace" key="Favorites" component={Search}/>
-    <Scene type="replace" key="Search" component={Search} />
-=======
     <Scene type="push" key="Search" initial={true} direction='horizontal' component={Search} duration={100} title='Search' />
     <Scene type="push" key="SignUp"component={Signup} direction='horizontal' duration={100} title='Signup' />
     <Scene type="push" key="Results" component={Results} direction='horizontal' duration={100} title='Results'backTitle="Search" panHandlers={null}/>
     <Scene type="push" key="Login"  direction='horizontal' component={Login} title='Login'backTitle="Signup" />
->>>>>>> testing routes
   </Scene>
 );
 
