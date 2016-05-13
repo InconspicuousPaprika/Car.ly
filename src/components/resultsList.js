f50332d7cffd902ac35d0ccf135641d2dd28b79f
Claire Hsu <clairehsu86@gmail.com>
2016-05-12 19:38:23 -0700

    recale images

--- src/components/resultsList.js
+++ src/components/resultsList.js
@@ -10,8 +10,9 @@ import React, {
   TouchableWithoutFeedback,
 } from 'react-native';
 import Immutable from 'immutable';
-import { connect } from 'react-redux';
 import _ from 'underscore';
+import { connect } from 'react-redux';
+
 
 @connect(state => ({
   query: state.search,
@@ -21,20 +22,39 @@ import _ from 'underscore';
   CAR_MAKES_AND_MODELS: state.search.allCars
 }))
 
-export default class PhotoList extends Component {
+export default class ResultsList extends Component {
+  static propTypes = {
+    dispatch: React.PropTypes.func.isRequired,
+    query: React.PropTypes.object.isRequired,
+    results: React.PropTypes.object.isRequired,
+    login: React.PropTypes.object.isRequired,
+    signup: React.PropTypes.object.isRequired
+  };
 
-  constructor(props){
-    super(props);
+  // constructor(props){
+  //   super(props);
+  // }
 
+  convertScale(url) {
+    // let newurl = url.replace(new RegExp('scaler\/\d\d\d\/\d\d\d'), "scaler/100/75"); ReGex is not happening
+    let newurl = 'http://images.autotrader.com/scaler/400/300/'+url.slice(42);
+    console.log(newurl);
+    // console.log('begin printing this.props');
+    // console.log(this.props);
+    // console.log('done printing this.props');
+    return newurl;
   }
 
   render() {
+    const { query, convertScale, results, login, signup, dispatch, CAR_MAKES_AND_MODELS } = this.props;
     console.log("results");
     console.log(this.props.results);
+    console.log("this.props");
+    console.log(this.props);
     return (
       // <View>
       // <Text>words</Text>
-      <ScrollView>
+      <ScrollView style={{flex: 1, backgroundColor: '#f2f2f2'}}>
         {
           _.map(this.props.results, function(item) {
             return (
@@ -43,14 +63,14 @@ export default class PhotoList extends Component {
                 <Image
                   key={`PhotoItem_${item["VEHICLETITLE LINK"]["0"].title}`}
                   style={styles.image}
-                  source={{uri: item.image["0"].src}}
+                  source={{uri: this.convertScale(item.image["0"].src)}}
                 />
                 <Text>{item["VEHICLETITLE LINK"]["0"].text}</Text>
                 <Text>{item["PRIMARY PRICE"]["0"].text}</Text>
                 </View>
               </TouchableWithoutFeedback>
             );
-          })
+          }, this)
         }
       </ScrollView>
       // </View>
