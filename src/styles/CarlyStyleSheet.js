01d7f0dff1708526fe579c08cef1c294bdad5ea6
Biletnikoff <bogeresanders@gmail.com>
2016-05-18 16:15:51 -0700

    new css sheets

---
+++ src/styles/CarlyStyleSheet.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import {StyleSheet, Platform} from 'react-native';
+
+module.exports = {
+  create(styles: Object): {[name: string]: number} {
+  const platformStyles = {};
+  Object.keys(styles).forEach((name) => {
+    let {ios, android, ...style} = {...styles[name]};
+    if (ios && Platform.OS === 'ios') {
+      style = {...style, ...ios};
+    }
+    if (android && Platform.OS === 'android') {
+      style = {...style, ...android};
+    }
+    platformStyles[name] = style;
+  });
+  return StyleSheet.create(platformStyles);
+ }
+
+}
