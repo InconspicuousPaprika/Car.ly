74cebd2af83a4032a5976aa7b238757b3eea51dc
Biletnikoff <bogeresanders@gmail.com>
2016-05-18 16:15:15 -0700

    add catch error handle

--- src/utils/getResponse.js
+++ src/utils/getResponse.js
@@ -2,10 +2,12 @@ import Promise from 'bluebird';
 
 export default function getResponse(response) {
   return Promise.resolve(response)
-    .then((res) => res);
+    .then((res) => res)
+    .catch((err) => console.log(err));
 }
 
 export default function getResponseJSON(response) {
   return Promise.resolve(response.json())
-    .then((res) => res);
+    .then((res) => res)
+    .catch((err) => console.log(err));
 }
