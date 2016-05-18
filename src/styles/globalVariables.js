59ca7b4874b5e05f92fa008993ae92946fb5e2a3
Biletnikoff <bogeresanders@gmail.com>
2016-05-18 16:11:53 -0700

    add new css

--- src/styles/globalVariables.js
+++ src/styles/globalVariables.js
@@ -5,4 +5,10 @@ module.exports = {
   orange: '#ff6204',
   background: '#f5f5f5',
   textColor: '#666',
+  actionText: '#3FB4CF',
+  inactiveText: '#9B9B9B',
+  darkText: '#032250',
+  lightText: '#7F91A7',
+  cellBorder: '#EEEEEE',
+  darkBackground: '#183E63',
 }
