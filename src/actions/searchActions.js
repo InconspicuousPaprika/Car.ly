c10eed4cdac1cd41856c2aa4fee4a4d7fb472497
Claire Hsu <clairehsu86@gmail.com>
2016-05-19 17:29:02 -0700

    implemented trucar extractor

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -5,7 +5,7 @@ import backend from '../service/backend.js';
 export default createAction(
   'GET_CARDATA',
   async (searchQuery) => {
-    var rawResults = await Promise.all([backend.getAutoTempestData(searchQuery)]);
+    var rawResults = await Promise.all([backend.getAutoTempestData(searchQuery), backend.getTrueCarData(searchQuery)]);
     var combinedResults = rawResults.reduce((a,b)=>a.extractorData.data[0].group.concat(b.extractorData.data[0].group));
-    return combinedResults.extractorData.data[0].group;
+    return combinedResults;
   });
