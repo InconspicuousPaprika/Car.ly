132af7d22124e44bb44a97c14d69e8323bced6de
Biletnikoff <bogeresanders@gmail.com>
2016-07-08 15:16:13 -0700

    heroku endpoint

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  user: 'root',
-  password: '',
-  database: 'carly'
+  host: 'us-cdbr-iron-east-04.cleardb.net'
+  user: 'b6684b5c440935',
+  password: 'e18bc25d',
+  database: 'heroku_de74eb90197ead9'
 });
+
 
 connection.connect();
 
