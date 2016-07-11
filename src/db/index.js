1aaeb882f5f6bff6ee0ef76cde4a3f5cd5bce3fb
Bo Sanders <bogeresanders@gmail.com>
2016-07-11 10:05:03 -0700

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'us-cdbr-iron-east-04.cleardb.net'
+  host: 'us-cdbr-iron-east-04.cleardb.net',
   user: 'b6684b5c440935',
   password: 'e18bc25d',
   database: 'heroku_de74eb90197ead9'
